ad7817143ef82ecfb3fc26523267b79ea53cf365
LiliaDiak <lilidiakovychhh@gmail.com>
2023-01-20 18:07:45 +0200

    third commit: added weather for 3 days + remade footers buttons to big one. Made functions for get icons, period  of the day, get weekday and ect

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ key = "f22d12ba1b50d6fbe65d82aebbf4ba73"
 
 
 let weatherDiv = document.querySelector(".weater__forecast");
-let weatherMain = document.querySelector(".weather__main")
+let weatherMain = document.querySelectorAll(".weather__main")
 let cityInput = document.querySelector("#cityInput");
 let cityBtn = document.querySelector("#input");
 let weatherwrapperforceveralDays = document.querySelectorAll(".weather__main-forecast");
-let weatherDay = document.querySelectorAll(".weather__main-date")
+let weatherDay = document.querySelectorAll(".weather__main-date");
+let footerBtnFooter3 = document.querySelector(".footer__btn-more-3");
+// let footerBtnFooter5 = document.querySelector(".footer__btn-more-5");
 
 function convertion(val) {
     return Math.round((val - 273).toFixed(2))
@@ -36,15 +38,39 @@ weatherIcon = (data) => {
         return "weatherIcons/else.svg"
     }
 }
+weatherIconForSerevalDays = (data, i)=>{
+    let time = (data.list[i].dt_txt).slice(11, 13);
+    main = data.list[i].weather[0].main;
+    if (main.toLowerCase() == "Snow".toLowerCase()) {
+        return "weatherForSeveralDays/snow.svg"
+    }
+    if (main.toLowerCase() == "rain".toLowerCase()) {
+        return "weatherForSeveralDays/rain.svg"
+    }
+    if (main.toLowerCase() == "thunderstorm".toLowerCase()) {
+        return "weatherForSeveralDays/storm.svg"
+    }
+    if (main.toLowerCase() == "Clouds".toLowerCase()) {
+        return "weatherForSeveralDays/Clouds.svg"
+    }
+    if (main.toLowerCase() == "drizzle".toLowerCase()) {
+        return "weatherForSeveralDays/Drizzle.svg"
+    }
+    if (main.toLowerCase() == "Clear".toLowerCase()) {
+        if((time=="00" || time =="03")){
+            return "weatherForSeveralDays/clearMoon.svg"
+        }if(time=="21" || time=="18"){
+            return "weatherForSeveralDays/SunAndMoom.svg"
+        }
+        else{
+            return "weatherForSeveralDays/Clear.svg"
+        }
+    }
+    else {
+        return "weatherForSeveralDays/else.svg"
+    }
+}
 timeSlice = (val) => val.slice(10, 16)
-
-cityBtn.addEventListener("click", function () {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&cnt=8&appid=f22d12ba1b50d6fbe65d82aebbf4ba73`)
-        .then(res => res.json())
-        .then(datas => {
-            console.log(datas)
-        })
-})
 
 getWeekday=(data, i)=>{
     let date = new Date(data.list[i].dt_txt);
@@ -68,16 +94,34 @@ getMonth=(data, i)=>{
     }
 }
 
-cityBtn.addEventListener("click", function () {
+getDayTime=(data, i)=>{
+    let time = (data.list[i].dt_txt).slice(11, 13);
+    console.log(time)
+    if(time == "03" || time == "00"){
+        return "night"
+    }
+    if(time == "09"|| time == "06"){
+        return "morning"
+    }
+    if(time == "15" || time == "12"){
+        return "day"
+    }
+    if(time == "21" || time =="18"){
+        return "evening"
+    }
+
+}
+
+cityBtn.addEventListener("click", function (e) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&cnt=8&appid=f22d12ba1b50d6fbe65d82aebbf4ba73`)
         .then(res => res.json())
         .then(data => {
             document.querySelector(".weather__city-name").innerHTML = cityInput.value;
-            weatherMain.style.cssText = `
+            weatherMain[0].style.cssText = `
             background-image: url(weather_bc.png);
             background-repeat: no-repeat;
             min-height: 192px;`;
-            weatherMain.innerHTML = `
+            weatherMain[0].innerHTML = `
             <div class="weather__current-wrapper">
             <div class="weather__current-up">
                 <p class="weather__current-temp">${convertion(data.main.temp)}°</p>
@@ -92,12 +136,19 @@ cityBtn.addEventListener("click", function () {
             </div>
         </div>`
         })
+
 })
 
-let creatWeatherfor3 = (data) => {
+let creatWeatherforseveralDays = (data, num) => {
     let dateAll = new Date();
     let date = 0;
     let day = dateAll.getDate();
+    weatherDay[0].innerHTML=" ";
+    weatherDay[1].innerHTML=" ";
+    weatherDay[2].innerHTML=" ";
+    weatherwrapperforceveralDays[0].innerHTML =" ";
+    weatherwrapperforceveralDays[1].innerHTML =" ";
+    weatherwrapperforceveralDays[2].innerHTML =" ";
     for (let i = 0; i < 30; i += 2) {
         date = ((data.list[i].dt_txt).slice(8, 11));
         if (+(day + 1) == date) {
@@ -106,15 +157,15 @@ let creatWeatherfor3 = (data) => {
             <p class="weather__main-month">${getMonth(data, i)}</p>`
             weatherwrapperforceveralDays[0].innerHTML += `
             <div class="weather__main-weather">
-                                        <p class="weather__main-time">night</p>
-                                        <img src="icon.svg" alt="">
+                                        <p class="weather__main-time">${getDayTime(data, i)}</p>
+                                        <img src="${weatherIconForSerevalDays(data, i)}" alt="">
                                         <div class="weather__main-temp-wrapper">
                                             <p class="weather__main-min">min: ${convertion(data.list[i].main.temp_min)}</p>
                                             <p class="weather__main-max">max: ${convertion(data.list[i].main.temp_max)}</p>
                                         </div>
                                         <div class="weather__main-feelslike-wrapper">
                                             <p class="weather__main-feelslike">feels like: ${convertion(data.list[i].main.feels_like)}</p>
-                                            <p class="weather__main-main">${convertion(data.list[i].weather[0].main)}</p>
+                                            <p class="weather__main-main">${data.list[i].weather[0].main}</p>
                                         </div>
                                     </div>
             `
@@ -125,15 +176,15 @@ let creatWeatherfor3 = (data) => {
             <p class="weather__main-month">${getMonth(data, i)}</p>`
             weatherwrapperforceveralDays[1].innerHTML += `
             <div class="weather__main-weather">
-                                        <p class="weather__main-time">night</p>
-                                        <img src="icon.svg" alt="">
+                                        <p class="weather__main-time">${getDayTime(data, i)}</p>
+                                        <img src="${weatherIconForSerevalDays(data, i)}" alt="">
                                         <div class="weather__main-temp-wrapper">
                                             <p class="weather__main-min">min: ${convertion(data.list[i].main.temp_min)}</p>
                                             <p class="weather__main-max">max: ${convertion(data.list[i].main.temp_max)}</p>
                                         </div>
                                         <div class="weather__main-feelslike-wrapper">
                                             <p class="weather__main-feelslike">feels like: ${convertion(data.list[i].main.feels_like)}</p>
-                                            <p class="weather__main-main">${convertion(data.list[i].weather[0].main)}</p>
+                                            <p class="weather__main-main">${data.list[i].weather[0].main}</p>
                                         </div>
                                     </div>
             `
@@ -144,15 +195,54 @@ let creatWeatherfor3 = (data) => {
             <p class="weather__main-month">${getMonth(data, i)}</p>`
             weatherwrapperforceveralDays[2].innerHTML += `
             <div class="weather__main-weather">
-                                        <p class="weather__main-time">night</p>
-                                        <img src="icon.svg" alt="">
+                                        <p class="weather__main-time">${getDayTime(data, i)}</p>
+                                        <img src="${weatherIconForSerevalDays(data, i)}" alt="">
                                         <div class="weather__main-temp-wrapper">
                                             <p class="weather__main-min">min: ${convertion(data.list[i].main.temp_min)}</p>
                                             <p class="weather__main-max">max: ${convertion(data.list[i].main.temp_max)}</p>
                                         </div>
                                         <div class="weather__main-feelslike-wrapper">
                                             <p class="weather__main-feelslike">feels like: ${convertion(data.list[i].main.feels_like)}</p>
-                                            <p class="weather__main-main">${convertion(data.list[i].weather[0].main)}</p>
+                                            <p class="weather__main-main">${data.list[i].weather[0].main}</p>
+                                        </div>
+                                    </div>
+            `
+        }
+
+        if ((+(day + 4) == date)&&(num==5)) {
+            weatherDay[3].innerHTML = `<h5 class="weather__main-title">${getWeekday(data, i)}</h5>
+            <p class="weather__main-num">${date}</p>
+            <p class="weather__main-month">${getMonth(data, i)}</p>`
+            weatherwrapperforceveralDays[3].innerHTML += `
+            <div class="weather__main-weather">
+                                        <p class="weather__main-time">${getDayTime(data, i)}</p>
+                                        <img src="${weatherIconForSerevalDays(data, i)}" alt="">
+                                        <div class="weather__main-temp-wrapper">
+                                            <p class="weather__main-min">min: ${convertion(data.list[i].main.temp_min)}</p>
+                                            <p class="weather__main-max">max: ${convertion(data.list[i].main.temp_max)}</p>
+                                        </div>
+                                        <div class="weather__main-feelslike-wrapper">
+                                            <p class="weather__main-feelslike">feels like: ${convertion(data.list[i].main.feels_like)}</p>
+                                            <p class="weather__main-main">${data.list[i].weather[0].main}</p>
+                                        </div>
+                                    </div>
+            `
+        }
+        if ((+(day + 5) == date)&&(num==5)) {
+            weatherDay[4].innerHTML = `<h5 class="weather__main-title">${getWeekday(data, i)}</h5>
+            <p class="weather__main-num">${date}</p>
+            <p class="weather__main-month">${getMonth(data, i)}</p>`
+            weatherwrapperforceveralDays[4].innerHTML += `
+            <div class="weather__main-weather">
+                                        <p class="weather__main-time">${getDayTime(data, i)}</p>
+                                        <img src="${weatherIconForSerevalDays(data, i)}" alt="">
+                                        <div class="weather__main-temp-wrapper">
+                                            <p class="weather__main-min">min: ${convertion(data.list[i].main.temp_min)}</p>
+                                            <p class="weather__main-max">max: ${convertion(data.list[i].main.temp_max)}</p>
+                                        </div>
+                                        <div class="weather__main-feelslike-wrapper">
+                                            <p class="weather__main-feelslike">feels like: ${convertion(data.list[i].main.feels_like)}</p>
+                                            <p class="weather__main-main">${data.list[i].weather[0].main}</p>
                                         </div>
                                     </div>
             `
@@ -161,14 +251,28 @@ let creatWeatherfor3 = (data) => {
 }
 
 
+footerBtnFooter3.addEventListener("click", function(e){
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityInput.value}&cnt=30&appid=f22d12ba1b50d6fbe65d82aebbf4ba73`)
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data);
+            creatWeatherforseveralDays(data, 3)
+        })
 
-fetch('https://api.openweathermap.org/data/2.5/forecast?q=london&cnt=30&appid=f22d12ba1b50d6fbe65d82aebbf4ba73')
-    .then(res => res.json())
-    .then(data => {
-        // console.log(data);
-        creatWeatherfor3(data)
-    })
+        if(footerBtnFooter3.classList.contains("footer__btn-more-active")){
+            footerBtnFooter3.classList.remove("footer__btn-more-active")
+        }
+})
 
+
+// footerBtnFooter5.addEventListener("click", function(){
+//     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Lviv&cnt=50&appid=f22d12ba1b50d6fbe65d82aebbf4ba73`)
+//         .then(res => res.json())
+//         .then(data => {
+//             // console.log(data);
+//             creatWeatherforseveralDays(data, 5)
+//         })
+// })
 
 
 
@@ -188,12 +292,5 @@ footerBtn.addEventListener("click", footer);
 function footer(event) {
     if (event.target.closest('.footer__btn')) {
         footerBtnMore3.classList.toggle('footer__btn-more-active');
-        footerBtnMore5.classList.toggle('footer__btn-more-active');
     }
 }
-
-
-
-
-
-
